f1089a1266e4271ddde52af025fe28d5e3f03d97
Mauricio Luan <mauricioluan2023@gmail.com>
2025-09-15 14:11:12 -0300

    refactor: refatorando exceções e retornos

--- app/core/get_data.py
+++ app/core/get_data.py
@@ -1,23 +1,23 @@
 import requests
+from app.core.exceptions import TomticketApiError
 
 
 def get_ticket(url, id_ticket, header):
     try:
         response = requests.get(f"{url}{id_ticket}", headers=header)
         response.raise_for_status()
-        ticket_data = response.json()
-
-        return ticket_data
-    except requests.exceptions.RequestException as e:
-        return {"Erro": f"Erro ao obter os dados do ticket: {e}"}
+        return response.json()
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            raise TomticketApiError(f"Ticket {id_ticket} nao encontrado.")
+        else:
+            raise TomticketApiError(f"Erro ao obter os dados do ticket: {id_ticket}")
 
 
 def get_customer(url, internal_id, header):
     try:
         response = requests.get(f"{url}{internal_id}", headers=header)
         response.raise_for_status()
-        customer_data = response.json()
-
-        return customer_data
-    except requests.exceptions.RequestException as e:
-        return {"Erro": f"Erro ao obter os dados do customer: {e}"}
+        return response.json()
+    except requests.exceptions.RequestException:
+        raise TomticketApiError(f"Erro ao obter os dados do cliente {internal_id}.")
--- app/main.py
+++ app/main.py
@@ -22,9 +22,10 @@ __status__ = "Development"
 
 
 from app.schemas.Ficha import InputUsuario
+from app.core.exceptions import TomticketApiError
 from app.core.logic import gera_ficha
 from app.core.gera_xlsx import gera_planilha
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 import io
 
@@ -43,16 +44,22 @@ def raiz():
 
 @app.post("/fichas/")
 def cria_ficha(input_usuario: InputUsuario):
-    ficha = gera_ficha(input_usuario)
-    workbook = gera_planilha(ficha)
-    nome_arquivo = f"{ficha.store}.xlsx"
+    try:
+        ficha = gera_ficha(input_usuario)
+        workbook = gera_planilha(ficha)
+        nome_arquivo = f"{ficha.store}.xlsx"
 
-    buffer = io.BytesIO()
-    workbook.save(buffer)
-    buffer.seek(0)
+        buffer = io.BytesIO()
+        workbook.save(buffer)
+        buffer.seek(0)
 
-    return StreamingResponse(
-        buffer,
-        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        headers={"Content-Disposition": f"attachment; filename={nome_arquivo}"},
-    )
+        return StreamingResponse(
+            buffer,
+            media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            headers={"Content-Disposition": f"attachment; filename={nome_arquivo}"},
+        )
+    except TomticketApiError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+        raise HTTPException(status_code=500, detail="Erro interno do servidor.")
